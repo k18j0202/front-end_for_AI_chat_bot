970562dfcaf78e4cfa244c9e7d52ea9c90c02f70
Karthik J <150527143+k18j0202@users.noreply.github.com>
2025-06-09 11:57:40 +0530

    Update Header.jsx
    Again login to sign in changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
           New account
         </a>
         <Button className="hidden lg:flex" href="#login">
-          Login
+          Sign in
         </Button>
 
         <Button
